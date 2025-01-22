cd46e2557d507cab24a96108e469391b71fad8e5
SagarNRao <sagarnrao154@gmail.com>
2025-01-22 15:28:32 +0530

    updated eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint-define-config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "no-unused-vars": "off",
-    },
+export default defineConfig({
+  extends: ['next/core-web-vitals', 'next/typescript'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
-];
-
-export default eslintConfig;
+});
